a6fe476e351432981bf741363f80ac34a5f6155c
Wilian <wilianzilv@gmail.com>
2023-02-05 01:15:34 -0300

    refactor(where_builder): remove código desnecessário

--- rps_databases/where_builder.py
+++ rps_databases/where_builder.py
@@ -1,21 +1,31 @@
+import pandas as pd
 from .operators import (
     Operator,
     Is
 )
 
 def build_where(**kwargs):
+
     conditions = []
     params = []
-    for key, value in kwargs.items():
-        if not isinstance(value, Operator):
-            value = Is(value)
 
-        conditions.append(f"{key} {value.build()}")
-        params.extend(value.params())
+    for key, operator in kwargs.items():
+        if not isinstance(operator, Operator):
+            if isinstance(operator, pd.DataFrame):
+
+                assert key in operator.columns, f"'{key}' não existe no DataFrame"
+                operator = operator.get(key)
+
+            operator = Is(operator).custom
+
+        conditions.append(f"{key} {operator.build()}")
+        params.extend(operator.params())
+
     where = " and ".join(conditions)
 
     if not len(where):
         return where, params
 
     where = "WHERE " + where
+
     return where, params
