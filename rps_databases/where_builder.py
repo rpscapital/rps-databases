13f128ac3f6c256ade6ba9100edc1a6c7e8cfebb
Wilian <wilianzilv@gmail.com>
2023-02-06 23:31:21 -0300

    refactor(where_builder): melhorias na lógica

--- rps_databases/where_builder.py
+++ rps_databases/where_builder.py
@@ -1,31 +1,86 @@
 import pandas as pd
-from .operators import (
+import typing
+from rps_databases.operators import (
     Operator,
-    Is
+    Is,
+    And,
+    Or
 )
 
-def build_where(**kwargs):
+def build_conditions(conditions: typing.Union[list, dict], condition="and"):
 
-    conditions = []
+    clauses = []
     params = []
 
-    for key, operator in kwargs.items():
-        if not isinstance(operator, Operator):
-            if isinstance(operator, pd.DataFrame):
+    if isinstance(conditions, And):
+        condition = 'and'
 
-                assert key in operator.columns, f"'{key}' não existe no DataFrame"
-                operator = operator.get(key)
+    if isinstance(conditions, Or):
+        condition = 'or'
 
-            operator = Is(operator).custom
+    if isinstance(conditions, list):
+        for item in conditions:
 
-        conditions.append(f"{key} {operator.build()}")
-        params.extend(operator.params())
+            local_clause, local_params = build_conditions(item)
 
-    where = " and ".join(conditions)
+            clauses.append(local_clause)
+            params.extend(local_params)
 
-    if not len(where):
-        return where, params
 
-    where = "WHERE " + where
+    if isinstance(conditions, dict):
+
+        local_clauses = []
+
+        if isinstance(conditions, list):
+
+            local_clause, local_params = build_conditions(conditions)
+
+            local_clauses.append(local_clause)
+            params.extend(local_params)
+
+        for key, operator in conditions.items():
+
+            if key in ['_']:
+
+                if not isinstance(operator, (And, Or)):
+                    raise Exception('num sei num sei num sei')
+
+                local_clause, local_params = build_conditions(operator, operator.condition)
+
+                local_clauses.append(local_clause)
+                params.extend(local_params)
+
+                continue
+
+            if not isinstance(operator, Operator):
+
+                if isinstance(operator, pd.DataFrame):
+
+                    assert key in operator.columns, f"'{key}' não existe no DataFrame"
+                    operator = operator.get(key)
+
+                operator = Is(operator).custom
+
+            local_clauses.append(f"{key} {operator.build()}")
+            params.extend(operator.params())
+
+        local_clause = f" {condition} ".join(local_clauses)
+
+        clauses.append(local_clause)
+
+    return f'({f" {condition} ".join(clauses)})', params
+
+def build_where(conditions):
+
+    if not isinstance(conditions, (And, Or)):
+        if isinstance(conditions, dict):
+            conditions = And(**conditions)
+
+    conditions, params = build_conditions(conditions)
+
+    if conditions == '()':
+        return '', []
+
+    where = "WHERE " + conditions
 
     return where, params
